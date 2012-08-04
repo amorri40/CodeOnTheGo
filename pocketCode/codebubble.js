b25744b1a84a3fec7fd7e455154196f3f4ed30fd
Alasdair Morrison <amorri40@gmail.com>
2012-08-04 02:56:06 +0100

    Split up into css and javascript files, iphone selection now works without done button, split up syntax blocks into individual files

---
+++ pocketCode/codebubble.js
@@ -0,0 +1,42 @@
+/*get the data from a webpage*/
+function loadPage(name,el) {
+	var returndata;
+$.get(name, function(data){
+el.innerHTML=data;
+});
+}
+
+/* Manually polling the selected index function for iphone Thanks to InvisibleBacon on StackOverflow */
+$.fn.quickChange = function(handler) {
+    return this.each(function() {
+        var self = this;
+        self.qcindex = self.selectedIndex;
+        var interval;
+        function handleChange() {
+            if (self.selectedIndex != self.qcindex) {
+                self.qcindex = self.selectedIndex;
+                handler.apply(self);
+            }
+        }
+        $(self).focus(function() {
+            interval = setInterval(handleChange, 100);
+        }).blur(function() { window.clearInterval(interval); })
+        .change(handleChange); //also wire the change event in case the interval technique isn't supported (chrome on android)
+    });
+};
+
+//scroll to element script
+(function($) {
+	  $.fn.goTo = function() {
+		  var scrollX = window.pageXOffset; var scrollY = window.pageYOffset;
+		  window.scrollTo($(this).offset().left-70,$(this).offset().top-130);
+		  return this; // for chaining...
+	  }
+  })(jQuery);
+  
+/* load the statements page initially */
+$(document).ready(function() {loadPage("./SyntaxBlocks/python/statements.html", document.getElementById('tooltippanel'));
+	   $("#syntaxcategory").quickChange(function() { 
+	   loadPage(this.options[this.selectedIndex].value, document.getElementById('tooltippanel'));
+	   });
+});
